cb3721ff703aa48e1daec3f73b300e4b733b56a3
Pauatro <pauatro@gmail.com>
2024-02-02 17:18:22 +0100

    refactor: update frontend local url

--- src/shared/settings.py
+++ src/shared/settings.py
@@ -5,5 +5,5 @@ from typing import List
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     BACKEND_CORS_ORIGINS: List[str] = [
-        "http://localhost:8000",
+        "http://localhost:5173",
     ]
