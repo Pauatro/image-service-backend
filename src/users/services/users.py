da5f1f5a8c8d4a9ca6e41893ccb3efa8cd525ef2
Pauatro <pauatro@gmail.com>
2024-02-08 14:23:52 +0100

    refactor: update functions for testing purposes and fix found issues

--- src/users/services/users.py
+++ src/users/services/users.py
@@ -1,7 +1,6 @@
-from sqlalchemy.orm import Session
 from sqlalchemy import select
 from shared.database import get_db_session
-from shared.logging import get_logger, Logger
+from shared.logging import get_logger
 from users.data.models import UserModel
 from users.services.schemas import User, CreateUser
 from users.services.exceptions import (
@@ -13,28 +12,28 @@ from users.services.authentication import verify_password
 
 
 def get_user_by_username(
-    username: str, session: Session = get_db_session(), logger: Logger = get_logger()
+    username: str, session=get_db_session(), logger=get_logger()
 ) -> User:
     try:
         statement = select(UserModel).where(UserModel.username == username)
         user = session.execute(statement).first()
-        if not user:
-            logger.error(f"User with username {username} was not found")
-            raise UserNotFoundException()
-        else:
-            logger.info(f"Found user {user[0].username} with id {user[0].id}")
-            return User.model_validate(user[0])
     except Exception as error:
         logger.error(error)
+        raise Exception(error)
+
+    if not user:
+        logger.warning(f"User with username {username} was not found")
+        raise UserNotFoundException()
+    else:
+        logger.info(f"Found user {user[0].username} with id {user[0].id}")
+        return User.model_validate(user[0])
 
 
-def create_user(
-    user: CreateUser, session: Session = get_db_session(), logger: Logger = get_logger()
-):
+def create_user(user: CreateUser, session=get_db_session(), logger=get_logger()):
     try:
         existing_user = get_user_by_username(user.username, session, logger)
         if existing_user:
-            logger.error(
+            logger.warning(
                 f"Failed to create user with username {user.username} because it already exists"
             )
             raise UserAlreadyExistsException()
@@ -46,11 +45,14 @@ def create_user(
 
         logger.info(f"Created user {new_user.username} with id {new_user.id}")
         return new_user
+    except UserAlreadyExistsException:
+        raise UserAlreadyExistsException()
     except Exception as error:
         logger.error(error)
+        raise Exception(error)
 
 
-def authenticate_user(user: User, password: str, logger: Logger = get_logger()):
+def authenticate_user(user: User, password: str, logger=get_logger()):
     try:
         if not verify_password(password, user.hashed_password):
             logger.info(f"Password authentication for user {user.id} failed")
